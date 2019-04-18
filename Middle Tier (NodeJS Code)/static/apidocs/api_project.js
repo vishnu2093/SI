70474e8edfc8a44dccad33d7b7279016ae11b01c
vishnu2093 <49768600+vishnu2093@users.noreply.github.com>
2019-04-18 19:05:41 -0400

    Add files via upload

---
+++ Middle Tier (NodeJS Code)/static/apidocs/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Find-the-Fix API Documentation",
+  "version": "1.0.0",
+  "description": "FtF API Documentation",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-06T06:35:03.626Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
